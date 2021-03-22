36fa6cbcb0eb54b9199fafe321f1ecd6954dec04
Cristy <cristy@no_spam.com>
2021-03-22 15:46:08 +0200

    Added view entry classes

---
+++ Emu5/DataViewEntry.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace Emu5
+{
+    /// <summary>
+    /// Interaction logic for DataViewEntry.xaml
+    /// </summary>
+    public partial class DataViewEntry : UserControl
+    {
+        public DataViewEntry()
+        {
+            InitializeComponent();
+        }
+
+        public void DisplayData(String baseAddress, String data0, String data1, String data2, String data3, String data4, String data5, String data6, String data7)
+        {
+            textBlockBaseAddress.Text = baseAddress;
+
+            textBlockData0.Text = data0;
+            textBlockData1.Text = data1;
+            textBlockData2.Text = data2;
+            textBlockData3.Text = data3;
+            textBlockData4.Text = data4;
+            textBlockData5.Text = data5;
+            textBlockData6.Text = data6;
+            textBlockData7.Text = data7;
+        }
+    }
+}
---
+++ Emu5/InstructionViewEntry.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+
+namespace Emu5
+{
+    /// <summary>
+    /// Interaction logic for InstructionViewEntry.xaml
+    /// </summary>
+    public partial class InstructionViewEntry : UserControl
+    {
+        public InstructionViewEntry()
+        {
+            InitializeComponent();
+        }
+
+        public void DisplayData(bool breakpoint, String address, String instruction, String rawValue, String annotation)
+        {
+            buttonToggleBreakpoint.Content = breakpoint ? new Ellipse { Height = 11, Width = 11 } : null;
+
+            textBlockAddress.Text = address;
+            textBlockInstruction.Text = instruction;
+            textBlockRawValue.Text = rawValue;
+            textBlockAnnotation.Text = annotation;
+        }
+    }
+}
