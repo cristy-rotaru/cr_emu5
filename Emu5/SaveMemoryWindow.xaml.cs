201cb5b4cd51beed4ac38e00a36e7d13a0d4ee91
Cristy <cristy@no_spam.com>
2021-05-14 14:51:28 +0300

    Added Save Memory window

---
+++ Emu5/SaveMemoryWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows;
+
+namespace Emu5
+{
+    /// <summary>
+    /// Interaction logic for SaveMemoryWindow.xaml
+    /// </summary>
+    public partial class SaveMemoryWindow : Window
+    {
+        public SaveMemoryWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
