aff9403048bdc132851a1319d646a7ab399c2665
Cristy <cristy@no_spam.com>
2021-05-26 14:10:42 +0300

    Added color selection item UI element

---
+++ Emu5/ColorSelectionItem.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Emu5
+{
+    /// <summary>
+    /// Interaction logic for ColorSelectionItem.xaml
+    /// </summary>
+    public partial class ColorSelectionItem : UserControl
+    {
+        public ColorSelectionItem(Brush color, String name)
+        {
+            InitializeComponent();
+
+            rectangleColorPreview.Fill = color;
+            textBlockColorName.Text = name;
+        }
+    }
+}
