1f5dc0750078c306e39183a527b3c49e8b0cbae2
Cristy <cristy@no_spam.com>
2021-05-30 12:54:11 +0300

    Checking settings validity at startup

--- Emu5/MainWindow.xaml.cs
+++ Emu5/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Globalization;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -17,11 +18,136 @@ namespace Emu5
             InitializeComponent();
         }
 
+        private void CheckSettingsValidity()
+        {
+            int l_fontSize = Properties.Settings.Default.editor_fontSize;
+            if (l_fontSize < 6 || l_fontSize > 48)
+            {
+                ResetSettingsToDefault();
+                return;
+            }
+
+            int l_programTemplate = Properties.Settings.Default.editor_defaultTemplate;
+            if (l_programTemplate < 0 || l_programTemplate > 2)
+            {
+                ResetSettingsToDefault();
+                return;
+            }
+
+            UInt32[] l_memoryRanges = Properties.Settings.Default.memoryMap_memoryRanges;
+            if (l_memoryRanges == null || l_memoryRanges.Length == 0 || (l_memoryRanges.Length & 1) != 0)
+            {
+                ResetSettingsToDefault();
+                return;
+            }
+            bool l_mainRangeFound = false;
+            for (int i_rangeIndex = 0; i_rangeIndex < l_memoryRanges.Length; i_rangeIndex += 2)
+            {
+                if (l_memoryRanges[i_rangeIndex] >= l_memoryRanges[i_rangeIndex + 1])
+                {
+                    ResetSettingsToDefault();
+                    return;
+                }
+                if (l_memoryRanges[i_rangeIndex] == 0x00000000 && l_memoryRanges[i_rangeIndex + 1] >= 0x80000)
+                {
+                    l_mainRangeFound = true;
+                }
+            }
+            if (l_mainRangeFound == false)
+            {
+                ResetSettingsToDefault();
+                return;
+            }
+
+            if (Properties.Settings.Default.emulator_useIntegratedEcallHandler)
+            {
+                UInt32 l_ecallBase = Properties.Settings.Default.emulator_ecallBase;
+                if (l_ecallBase < 0x10000000 || (l_ecallBase & 0x3) != 0)
+                {
+                    ResetSettingsToDefault();
+                    return;
+                }
+                MemoryStream l_ecallHandlerStream = new MemoryStream(Properties.Resources.ecall_handler);
+                UInt32 l_ecallEnd = l_ecallBase + (UInt32)l_ecallHandlerStream.Length - 1;
+                bool l_ecallRangeDefined = false;
+                for (int i_rangeIndex = 0; i_rangeIndex < l_memoryRanges.Length; i_rangeIndex += 2)
+                {
+                    if (l_memoryRanges[i_rangeIndex] <= l_ecallBase && l_memoryRanges[i_rangeIndex + 1] >= l_ecallEnd)
+                    {
+                        l_ecallRangeDefined = true;
+                    }
+                }
+                if (l_ecallRangeDefined == false)
+                {
+                    ResetSettingsToDefault();
+                    return;
+                }
+                if (Properties.Settings.Default.peripherals_enableTerminal == false)
+                {
+                    ResetSettingsToDefault();
+                    return;
+                }
+            }
+
+            int l_verbosity = Properties.Settings.Default.logging_verbosity;
+            if (l_verbosity < 0 || l_verbosity > 2)
+            {
+                ResetSettingsToDefault();
+                return;
+            }
+
+            if (Properties.Settings.Default.logging_enable)
+            {
+                if (Properties.Settings.Default.logging_dontLogEcall == true && Properties.Settings.Default.emulator_useIntegratedEcallHandler == false)
+                {
+                    ResetSettingsToDefault();
+                    return;
+                }
+            }
+        }
+
+        private void ResetSettingsToDefault()
+        {
+            MessageBox.Show("Invalid settings detected.\nResettings all settings to default.", "Settings corrupted", MessageBoxButton.OK, MessageBoxImage.Warning);
+
+            Properties.Settings.Default.editor_fontSize = 15;
+            Properties.Settings.Default.editor_enableHighlighting = true;
+            Properties.Settings.Default.editor_defaultTemplate = 1;
+
+            Properties.Settings.Default.emulator_clearMemoryMap = false;
+            Properties.Settings.Default.emulator_useIntegratedEcallHandler = true;
+            Properties.Settings.Default.emulator_ecallBase = 0xFFFFF000;
+
+            UInt32[] l_memoryRanges = new UInt32[4];
+            l_memoryRanges[0] = 0x00000000;
+            l_memoryRanges[1] = 0x9FFFFFFF;
+            l_memoryRanges[2] = 0xC0000000;
+            l_memoryRanges[3] = 0xFFFFFFFF;
+            Properties.Settings.Default.memoryMap_memoryRanges = l_memoryRanges;
+            Properties.Settings.Default.memoryMap_uninitializedMemoryValue = 0xFF;
+
+            Properties.Settings.Default.peripherals_enableIOPanel = true;
+            Properties.Settings.Default.peripherals_enableTerminal = true;
+            Properties.Settings.Default.peripherals_enableInterruptInjector = false;
+
+            Properties.Settings.Default.terminal_textColorIndex = 21;
+            Properties.Settings.Default.terminal_backgroundColorIndex = 0;
+
+            Properties.Settings.Default.logging_enable = true;
+            Properties.Settings.Default.logging_verbosity = 1;
+            Properties.Settings.Default.logging_clearOnNewSimulation = false;
+            Properties.Settings.Default.logging_dontLogEcall = true;
+
+            Properties.Settings.Default.Save();
+        }
+
         #region r_windowEvents
         private void MainWindow_Initialized(object sender, EventArgs e)
         {
             CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("En-US");
             CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("En-US");
+
+            CheckSettingsValidity();
 
             TabItem l_welcomeTab = new TabItem();
             l_welcomeTab.Header = new TabHeader("Welcome", false, () => RemoveTab(l_welcomeTab));
